6464cfc7640e1d7f5f575f885e6f0be606057b12
Heidi <hh360@cam.ac.uk>
2012-08-24 14:16:38 +0100

    adding server implementation

--- basicServer.sh
+++ basicServer.sh
@@ -3,6 +3,12 @@
 # for simplification, I will currently just generate TCP traffic, no UDP traffic
 # traffic will be generated with iperf
 
+
+iperf -s &
+pids=$!
+sleep 12
+kill $pids
+
 # the network traffic is measured using tcpdump
 
 iperf -c 192.168.14.175 -t 10 -r | sudo tcpdump -i wlan0 -w output_file dst or src 192.168.14.175 &
@@ -19,7 +25,3 @@ rm -f output_file
 
 echo "Test Complete"
 
-iperf -s &
-pids=$!
-sleep 12
-kill $pids
